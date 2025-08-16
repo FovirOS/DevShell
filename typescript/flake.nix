52d571ea810bb1c7a59219a44ed6c0041b8b5bfe
Fovir <fovir@disroot.org>
2025-08-16 19:23:13 +0800

    feat: Add dev shell for TypeScript.

---
+++ typescript/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "DevShell for TypeScript";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs
+      ];
+
+      shellHook = ''
+        export NODE_ENV=development
+
+        ensure_pnpm_package() {
+           local pkg=$1
+
+          if npm list --depth=0 | grep "$pkg" &>/dev/null; then
+            echo "$pkg installed"
+          else
+            echo "Installing $pkg..."
+            npm add "$pkg"
+          fi
+        }
+
+        ensure_pnpm_package tsx
+
+        echo "Entering the development environment!"
+        echo "Node: $(node -v), npx: $(npx -v)"
+
+        trap 'echo "Leaving the development environment!"' EXIT
+      '';
+    };
+  };
+}
