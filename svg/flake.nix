52a3020edde12d5eb6e70be0f27df828c76e6d3a
Fovir <fovir@disroot.org>
2025-08-22 16:51:37 +0800

    feat: Update the SVG devshell.

---
+++ svg/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "DevShell for SVG";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs
+      ];
+
+      shellHook = ''
+        export NODE_ENV=development
+
+        ensure_npm_package() {
+           local pkg=$1
+
+          if npm list --depth=0 | grep "$pkg" &>/dev/null; then
+            echo "$pkg installed"
+          else
+            echo "Installing $pkg..."
+            npm add "$pkg"
+          fi
+        }
+
+        ensure_npm_package prettier
+        ensure_npm_package @prettier/plugin-xml
+
+        echo "Entering the development environment!"
+        echo "Node: $(node -v), npm: $(npm -v)"
+
+        trap 'echo "Leaving the development environment!"' EXIT
+      '';
+    };
+  };
+}
