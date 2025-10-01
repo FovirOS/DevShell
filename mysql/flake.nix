50c44f2114765a2198cdb8f07a0dc8808f9d44e7
Fovir <fovir@disroot.org>
2025-10-01 18:27:47 +0800

    feat: Add Justfile.

--- mysql/flake.nix
+++ mysql/flake.nix
@@ -13,13 +13,19 @@
     devShells.${system}.default = pkgs.mkShell {
       # Add packages here.
       buildInputs = with pkgs; [
-        mysql84
+        mysql80
+        just
       ];
 
       # Shell hooks.
       shellHook = ''
         echo "Entering the development environment!"
         mysql --version
+
+        mysqld --initialize-insecure --datadir ./data
+        mysqld --datadir ./data --socket ./data/mysql.sock &
+
+        echo "Use `just start` to enter MySQL. "
 
         trap 'echo "Leaving the development environment!"' EXIT
       '';
